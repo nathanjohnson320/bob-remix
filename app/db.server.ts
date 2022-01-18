418939b65081b239c10a2f97f588adea0d8f6cec
Nathan Johnson <nathanjohnson320@gmail.com>
2022-01-17 21:27:03 -0500

    Fix fetch binding

--- app/db.server.ts
+++ app/db.server.ts
@@ -3,5 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 export const supabase = () => {
   // Globals are from cloudflare secrets
   // @ts-ignore
-  return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, { fetch });
+  return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    fetch: fetch.bind(globalThis),
+  });
 };
